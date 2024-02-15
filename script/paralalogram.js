59d7d8c0b51de80b3ef5a6e60251bd11e5c80697
Asikkhan48 <khanasik48@gmail.com>
2024-02-16 02:13:03 +0600

    more

---
+++ script/paralalogram.js
@@ -0,0 +1,31 @@
+
+// paralalogram
+
+
+// function CalculateParalalogramarea() {
+
+//     // Base
+//     const paralalogramInput = document.getElementById("parallelogram-base");
+//     const paralalogramText = paralalogramInput.value;
+//     const base = parseFloat(paralalogramText);
+
+//     // Height
+//     const paralalogramHeight = document.getElementById("paralalogram-height");
+//     const paralaloGramText = paralalogramHeight.value;
+//     const height = parseFloat(paralaloGramText);
+
+//     // calculate
+//     const area = base * height;
+
+//     // display
+//     const paralalogramAreaSpan = document.getElementById("parallelogram-area")
+//     paralalogramAreaSpan.innerText = area;
+// }
+
+function CalculateParalalogramarea() {
+
+}
+
+function getInputValueId(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+}
--- script/rectangle.js
+++ script/rectangle.js
@@ -1,3 +1,16 @@
 function CalculateRectangleArea(){
+    const lengthInput = document.getElementById("rectangle-width");
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+// height
+    const widthInput = document.getElementById("reactangle-height");
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
     
+    // calculate rectangle areas
+    const area = length * width;
+
+    // display Rectangle Area
+    const reactangleAreaSpan = document.getElementById("rectangle-area");
+    reactangleAreaSpan.innerText = area;
 }
